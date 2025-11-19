2ede92e7b6bdf82fd5ae3c3b6b2c19823c904f6c
Amal <68412140+AmalAliQ@users.noreply.github.com>
2025-11-20 01:32:28 +0300

    done

---
+++ oopProject/src/oopproject/Employee.java
@@ -0,0 +1,7 @@
+package oopproject;
+
+public abstract class Employee extends Person {
+    public Employee(String name, int id) {
+        super(name, id); }
+    public abstract double calculateSalary();
+}
